4f8baf4e277e062b8b1b8d09e97be7ff5cedd2be
mark8988 <mark@min-net.net>
2022-09-15 18:54:53 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-in17d8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-in17d8)
---
+++ src/app/hello-world-interpolation/hello-world-interpolation.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hello-world-interpolation',
+  templateUrl: './hello-world-interpolation.component.html',
+})
+export class HelloWorldInterpolationComponent {
+  message = 'Hello, World123!';
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
